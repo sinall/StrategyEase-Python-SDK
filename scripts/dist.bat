002948e452e4fcee2bf049bd44baba8f3397ea19
Gao Rui <gaoruinan@163.com>
2017-05-14 22:59:42 +0800

    Enhance scripts/dist.bat; add scripts/dist.sh

--- scripts/dist.bat
+++ scripts/dist.bat
@@ -1,3 +1,10 @@
+set CWD=%cd%
+set SCRIPT_DIR=%~dp0
+set HOME_DIR=%SCRIPT_DIR%..
+
+cd %HOME_DIR%
+del /s /q dist\*
 python setup.py sdist
 python setup.py bdist_wheel --universal
-twine upload dist/*
+twine upload dist\*
+cd %CWD%
---
+++ scripts/dist.sh
@@ -0,0 +1,10 @@
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR=$(dirname "$0")
+HOME_DIR=$SCRIPT_DIR/..
+
+cd $HOME_DIR
+rm -rf dist/*
+python3 setup.py sdist
+python3 setup.py bdist_wheel --universal
+twine upload dist/*
+cd $CWD
