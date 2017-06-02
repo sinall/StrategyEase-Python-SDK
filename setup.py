7865449cba86597ffe71c28c7b0fd2c3fecf5f18
Gao Rui <gaoruinan@163.com>
2017-06-02 12:05:25 +0800

    Release 1.1.0.a12

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='shipane_sdk',
 
-    version='1.1.0.a11',
+    version='1.1.0.a12',
 
     description=u'实盘易（ShiPanE）Python SDK，通达信自动化交易 API。',
     long_description=long_description,
