206e750dee2d38b03fbf9722cab5dbfe35de1e36
Gao Rui <gaoruinan@163.com>
2017-08-06 15:28:34 +0800

    Refactor ClientTest to support THS

--- tests/shipane_sdk/test_client.py
+++ tests/shipane_sdk/test_client.py
@@ -5,11 +5,13 @@ import os
 import unittest
 
 import six
+from hamcrest import *
 from requests import HTTPError
 from six.moves import configparser
 
 from shipane_sdk import Client
 from shipane_sdk.client import MediaType
+from tests.shipane_sdk.matchers.dataframe_matchers import *
 
 if six.PY2:
     ConfigParser = configparser.RawConfigParser
@@ -36,10 +38,8 @@ class ClientTest(unittest.TestCase):
     def test_get_positions(self):
         try:
             data = self.client.get_positions(self.client_param)
-            sub_accounts = data['sub_accounts']
-            self.assertGreaterEqual(sub_accounts[u'余额'][u'人民币'], 0)
-            positions = data['positions']
-            self.assertIsNotNone(positions[u'证券代码'][0])
+            assert_that(data['sub_accounts'], has_row(u'人民币'))
+            assert_that(data['positions'], has_column(u'证券代码'))
         except HTTPError as e:
             self.fail()
 
@@ -53,27 +53,27 @@ class ClientTest(unittest.TestCase):
     def test_get_orders(self):
         try:
             df = self.client.get_orders(self.client_param)
-            self.assertIsNotNone(df[u'委托编号'][0])
+            assert_that(df, has_column_matches(u"(委托|合同)编号"))
         except HTTPError as e:
             self.fail()
 
     def test_get_open_orders(self):
         try:
             df = self.client.get_orders(self.client_param, 'open')
-            self.assertIsNotNone(df[u'委托编号'][0])
+            assert_that(df, has_column_matches(u"(委托|合同)编号"))
         except HTTPError as e:
             self.fail()
 
     def test_get_filled_orders(self):
         try:
             df = self.client.get_orders(self.client_param, 'filled')
-            self.assertIsNotNone(df[u'委托编号'][0])
+            assert_that(df, has_column_matches(u"(委托|合同)编号"))
         except HTTPError as e:
             self.fail()
 
     def test_buy_stock(self):
         try:
-            order = self.client.buy(self.client_param, symbol='000001', price=8.11, amount=100)
+            order = self.client.buy(self.client_param, symbol='000001', price=11.11, amount=100)
             self.assertIsNotNone(order['id'])
         except HTTPError as e:
             result = e.response.json()
@@ -81,7 +81,7 @@ class ClientTest(unittest.TestCase):
 
     def test_sell_stock(self):
         try:
-            order = self.client.sell(self.client_param, symbol='000001', price=9.51, amount=100)
+            order = self.client.sell(self.client_param, symbol='000001', price=12.11, amount=100)
             self.assertIsNotNone(order['id'])
         except HTTPError as e:
             result = e.response.json()
@@ -96,7 +96,7 @@ class ClientTest(unittest.TestCase):
     def test_query(self):
         try:
             df = self.client.query(self.client_param, '查询>资金股份')
-            self.assertIsNotNone(df[u'证券代码'][0])
+            assert_that(df, has_column(u'证券代码'))
         except HTTPError as e:
             self.fail()
 
