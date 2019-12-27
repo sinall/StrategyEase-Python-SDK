143e0ac71fdc76f21738c03827e81477520ee3da
Gao Rui <gaoruinan@163.com>
2019-12-27 20:21:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -66,6 +66,8 @@ def main():
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
         ],
 
         keywords='StrategyEase SDK',
